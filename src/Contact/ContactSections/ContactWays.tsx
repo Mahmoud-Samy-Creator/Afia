6e728a44132cf26cf6a678f53fd353657b205a7d
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-12-02 14:41:28 +0200

    Re-design some components

--- src/Contact/ContactSections/ContactWays.tsx
+++ src/Contact/ContactSections/ContactWays.tsx
@@ -23,7 +23,12 @@ function ContactWays() {
       <div className="contact-ways w-[350px] content-center">
         {
           contactButtons.map(button =>
-            <button key={button.id} className="contact-button block text-center bg-[white] w-[250px] mx-[auto] mb-[15px] py-[10px] border border-[#e5e5e5] rounded-[10px] transition transiton-[0.3s] hover:bg-[#054f6bd4] hover:text-white">{button.content}</button>
+            <button
+              key={button.id}
+              className="contact-button block text-center text-[18px] bg-[white] w-[300px] h-[60px] mx-[auto] mb-[15px] py-[10px] border border-[#e5e5e5] rounded-[10px] transition transiton-[0.3s] hover:bg-[#054f6bd4] hover:text-white"
+            >{
+              button.content}
+            </button>
           )}
       </div>
   )
